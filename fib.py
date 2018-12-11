4881e5aa1e989dc873e07fd16ea35d412379fd30
Duy <hotboyarsenal@gmail.com>
2018-12-12 02:38:04 +0700

    update fib.py

--- fib.py
+++ fib.py
@@ -1,8 +1,9 @@
 def fib(n):
-    fib_list = [0] * n
-    fib_list[1] = 1
+    f = [0] * n
+    f[0] = 1 
+    f[1] = 1
     for i in range(2,n):
-        fib_list[i] = fib_list[i-1] + fib_list[i-2]
-    #return fib_list
-    print(fib_list)
-fib(int(input("Enter nunber element of list: ")))
+        f[i] = f[i-1] + f[i-2]
+    return f
+a = fib(int(input("Enter nunbers element of list: ")))
+print(a)
