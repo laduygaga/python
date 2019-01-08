f4ee294d3df938f096d744084a13f8513e3b8731
Duy <hotboyarsenal@gmail.com>
2019-01-09 03:23:58 +0700

    add banhchung.py

---
+++ banhchung.py
@@ -0,0 +1,12 @@
+def bc():
+    n = int(input("banhchung_size:"))
+    banhchung =[]
+    for i in range(n):
+        banhchung.append(n*['*'])
+    for i in range(n): 
+        for j in range(n):
+            if (i!=0 and i!=(n-1)) and j!=0 and j!=(n-1) and i!=j and j!=n-(i+1) and i!=(n//2) and j!=(n//2):
+                banhchung[i][j] = " "
+    for row in banhchung:
+        print(' '.join(row))
+bc()
