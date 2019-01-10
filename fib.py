a58be9f9a3350cb31987578c0a84852b8ecea054
Duy <hotboyarsenal@gmail.com>
2019-01-10 18:30:39 +0700

    updated fib.py

--- fib.py
+++ fib.py
@@ -9,7 +9,7 @@ fib()
 #def fib_1(n):
 #    a, b = 0, 1
 #    while b <= n:
-#        print(b, end=' ')
+#        print(b, end=" ")
 #        a, b = b, a + b
 #    print()
 #fib_1(n = int(input("Enter the nearest with last number :))")))
