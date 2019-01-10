4f4279986851795ab0aef4708ec4f0e7120909f9
Duy <hotboyarsenal@gmail.com>
2019-01-10 18:19:56 +0700

    updated fib.py

--- fib.py
+++ fib.py
@@ -1,16 +1,15 @@
-def fib(n):
+def fib(n = int(input("lenght of fib:"))): 
     f = [1] * n
     for i in range(2,n):
         f[i] = f[i-1] + f[i-2]
-    return f
-a = fib(int(input("Enter nunbers element of list: ")))
-print(a)
+    print(f)
+fib()
 
 
-#def fib(n):
+#def fib_1(n):
 #    a, b = 0, 1
-#    while b < n:
-#        print(b, end=" ")
+#    while b <= n:
+#        print(b, end=' ')
 #        a, b = b, a + b
 #    print()
-#fib(n = int(input("Enter the nearest with last number :))")))
+#fib_1(n = int(input("Enter the nearest with last number :))")))
