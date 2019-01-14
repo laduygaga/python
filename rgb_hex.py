2a83f2a3a6e0c2e557bc6fd790f73289057bf1e4
root <root@localhost.localdomain>
2019-01-14 09:16:48 -0500

    edit rgb_hex.py

--- rgb_hex.py
+++ rgb_hex.py
@@ -19,17 +19,17 @@ def rgb_hex():
 def hex_rgb():
     hex_val = input("Enter a hex value (6 digits): ")
     if len(hex_val) != 6:
-        print(invalid_msg)
-    return
+        print("invalid")
+        return
     else:
         hex_val = int(hex_val, 16)
-        two_hex_digits = 2**8
-        blue = hex_val % two_hex_digits
-        hex_val = hex_val >> 8
-        green = hex_val % two_hex_digits
-        hex_val = hex_val >> 8
-        red = hex_val % two_hex_digits
-        print("Red: %s Green: %s Blue: %s" % (blue, green, red))
+    two_hex_digits = 2**8
+    blue = hex_val % two_hex_digits
+    hex_val = hex_val >> 8
+    green = hex_val % two_hex_digits
+    hex_val = hex_val >> 8
+    red = hex_val % two_hex_digits
+    print("Red: %s Green: %s Blue: %s" % (blue, green, red))
 
 
 def convert():
@@ -37,16 +37,16 @@ def convert():
         option = input("Enter 1 to conovert RGB to HEX, \
                        Enter 2 to convert HEX to RGB, \
                        Enter X to Exit: ").upper()
-    if option == "1":
-        print("RGB to HEX... ")
-        rgb_hex()
-    elif option == "2":
-        print("HEX to RGB...")
-        hex_rgb()
-    elif option == "X":
-        break
-    else:
-        print("Error")
+        if option == "1":
+            print("RGB to HEX... ")
+            rgb_hex()
+        elif option == "2":
+            print("HEX to RGB...")
+            hex_rgb()
+        elif option == "X":
+            break
+        else:
+            print("Error")
 
 
 convert()
