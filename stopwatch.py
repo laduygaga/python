07cea38284918daf20fba0aca99a1a5e67503be6
Duy Nguyen <hotboyarsenal@gmail.com>
2019-09-27 13:33:50 +0700

    update stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -4,10 +4,11 @@ import sys
 def stopwatch():
     s = int(sys.argv[1])
     while s>=0:
-        print(s,sep='')
+        print(s,end ='\r')
+        # print(s,sep='')
         sleep(1)
         s -= 1
+    os.system('sxiv /home/duy/Pictures/dieu_in_toilet.png')        
     os.system('mpv /home/duy/Music/curtainCall/Eminem\ -\ FACK.mp3')
-        
 if __name__ == "__main__":
     stopwatch()
