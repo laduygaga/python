9dbbea90b9a7a4f2cb9d072d515d9fd216c40730
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-02-17 00:54:11 +0700

    update

--- diamond.py
+++ diamond.py
@@ -1,9 +1,6 @@
 def diamond(a):
-    b = abs(a) + 1
-    for i in range(-a, b):
+    for i in range(-a, a+1):
         print(abs(i) * ' ' + abs(2*abs(i) - (2*a+1)) * '*')
-        # or print(abs(i) * ' ' + abs(2abs(i) - a) * '*' + abs(abs(i) - b) * '*')
-
 
 if __name__ == "__main__":
-    diamond(int(input("Diamond size = ")))
+    diamond(abs(int(input("Diamond size = "))))
--- h_.py
+++ h_.py
@@ -1,15 +1,13 @@
-import sys
-import time
-import string
-import random
+import time, sys, string, random
 
-target = 'FUCK YOU, MOTHER FUCKER!!!'
+target = 'My name is NGUYEN NGOC DUY, FUCKER YOU MOTHER FUCKER'
 guess = ['']*len(target)
 charlist = string.ascii_letters + string.punctuation + " "
 while True:
     guess = [t if c == t else random.choice(charlist) for c, t in zip(guess, target)]
-    sys.stdout.write(f'\r{"".join(guess)}')
-    sys.stdout.flush()
+    print(f'{"".join(guess)}', end='\r')
+    # or sys.stdout.write(f'\r{"".join(guess)}')
+    # sys.stdout.flush()
     time.sleep(0.03)
     if ''.join(guess) == target:
         break
