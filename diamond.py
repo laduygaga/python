3662aa992227050900fd22e31daa27c6bcd109e2
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-02-16 22:33:21 +0700

    update

--- diamond.py
+++ diamond.py
@@ -1,7 +1,8 @@
 def diamond(a):
     b = abs(a) + 1
     for i in range(-a, b):
-        print(abs(i) * ' ' + abs(abs(i) - a) * '*' + abs(abs(i) - b) * '*')
+        print(abs(i) * ' ' + abs(2*abs(i) - (2*a+1)) * '*')
+        # or print(abs(i) * ' ' + abs(2abs(i) - a) * '*' + abs(abs(i) - b) * '*')
 
 
 if __name__ == "__main__":
