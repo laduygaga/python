650754c909b63aadff935d89cea61c4057b7161b
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-02-21 21:59:08 +0700

    update

--- learnE.py
+++ learnE.py
@@ -1,11 +1,13 @@
+import subprocess
 import os
 from time import sleep
 
-f = open('/home/duy/1000PW.txt', 'r')
+f = open('/home/duy/1000.txt', 'r')
 a = f.readlines()
 b = ''.join(a)
 c = b.split()
 for i in c:
     print(c.index(i), i)
-    os.system('goldendict {}'.format(i))
-    sleep(1)
+    os.system(f'goldendict {i}')
+    # subprocess.Popen(f"goldendict  {i}", shell = True)
+    sleep(.8)
