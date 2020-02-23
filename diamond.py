7d24d68c3e72eac54f9dbbc9a87262ae9b7906f9
Nguyen Ngoc Duy <hotboyarsenal@gmail.com>
2020-02-24 06:54:50 +0700

    update

--- diamond.py
+++ diamond.py
@@ -1,6 +1,6 @@
 def diamond(a):
     for i in range(-a, a+1):
-        print(abs(i) * ' ' + abs(2*abs(i) - (2*a+1)) * '*')
+        print(abs(i) * ' ' + abs( (2*a+1) - abs(2*i) ) * '*')
 
 if __name__ == "__main__":
     diamond(abs(int(input("Diamond size = "))))
